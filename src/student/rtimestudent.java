f4117514c9b2597c3406d487a73681ad7b4adfe5
Tylor <Ali355@sheridancollege.com>
2023-02-01 17:13:45 -0500

    rtimestudent

---
+++ src/student/rtimestudent.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package student;
+
+/**
+ *
+ * @author Andrew
+ */
+public class rtimestudent extends student {
+    private int getofCourse ; 
+
+    public int getGetofCourse() {
+        return getofCourse;
+    }
+
+    public void setGetofCourse(int getofCourse) {
+        this.getofCourse = getofCourse;
+    }}
+    
+    
